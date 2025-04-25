e09ea077f36abc1555bef5476896497f72980e7c
ElieOko <elieoko100@gmail.com>
2025-04-25 07:46:50 -0700

    fix:pluggins and config idea

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    kotlin("plugin.serialization") version "2.1.20"
 }
 
 android {
@@ -49,6 +50,38 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    //
+    implementation(libs.foundation.pager)
+    implementation(libs.androidx.navigation.compose)
+    //Coroutines
+    implementation (libs.kotlinx.coroutines.core)
+    implementation (libs.kotlinx.coroutines.android)
+    implementation (libs.kotlinx.coroutines.play.services)
+    // Kotlin Reflect (for reflection support)
+    implementation(libs.kotlin.reflect)
+    //Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx.v262)
+    implementation (libs.androidx.lifecycle.extensions)
+    implementation( libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation (libs.androidx.activity.ktx)
+    //Koin
+    implementation (libs.koin.android)
+    implementation (libs.koin.androidx.compose)
+    //Koil Image Dependency
+    implementation(libs.coil.compose)
+    //Ktor & kotlin Serialization
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.android.v2312)
+    implementation(libs.ktor.client.content.negotiation.v2312)
+    implementation(libs.ktor.client.android)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.client.logging.jvm)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
